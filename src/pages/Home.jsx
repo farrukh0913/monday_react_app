09611dd66dd505ca76c06a7a6bdf2194db34613e
farrukh0913 <farrukh.khan0913@gmail.com>
2023-10-10 23:54:25 +0500

    default route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "../components/Header";
 import DocGen from "../components/Docgen";
 import { Outlet, Link } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  useEffect(()=> {
+    if(window.location.pathname === '/'){
+      navigate("/preview");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Header></Header>
